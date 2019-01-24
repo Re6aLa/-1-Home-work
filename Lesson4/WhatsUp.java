87d73661219e675b7227e0c891ea9f9d5ec92f99
Danil Kristev <danishta_kristev@mail.ru>
2019-01-24 15:44:38 +0200

    Lesson4HomeWork# Lesson4HomeWorkwith '#' will be ignored, and an empty message aborts the commit. Lesson4HomeWork Lesson4HomeWorkOn branch master Lesson4HomeWorkYour branch is up to date with 'origin/master'. Lesson4HomeWork Lesson4HomeWorkChanges to be committed: Lesson4HomeWork      modified:   WhatsUp.class Lesson4HomeWork      modified:   WhatsUp.java Lesson4HomeWork

--- Lesson4/WhatsUp.java
+++ Lesson4/WhatsUp.java
@@ -3,17 +3,17 @@ public class WhatsUp {
 
 	public static void main(String[] args){
 		System.out.println("\nHello User");
-		System.out.println("\nLet's get to know each other");
+		System.out.println("\nLet's say as about you ");
 		System.out.println("\nWhat's your name: ");
 		System.out.println("\nWhat's your surname: ");
-		System.out.println("\nWhat's your Domein : ");
+		System.out.println("\nWhat's your activity : ");
 		Scanner reader = new Scanner(System.in);
 		String userName = reader.next();
 		String userSurname = reader.next();
-		String userDomein = reader.next();
+		String userActivity = reader.next();
 		reader.close();
-		System.out.println("\nNice to meet you, " + userName + userSurname);
-		System.out.println(	userDomein);
+		System.out.println("\nNice to meet you, " + userName + "  " +userSurname );
+		System.out.println(userActivity);
 		
 	}
 }
